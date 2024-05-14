ad07c2e9a58cbdfb8babdddca0d29ee1c2696a98
soloman-err <soloman.err@gmail.com>
2024-05-14 07:59:10 +0600

    hero layout update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -90,58 +90,60 @@ const Hero = () => {
             duration: 1,
             ease: "easeInOut",
           }}
-          className="z-1 absolute flex flex-col items-center justify-center h-[80vh]  text-center space-y-6 md:space-y-8 max-w-5xl mx-auto w-full px-4 mt-20"
+          className="z-1 absolute flex flex-col items-center justify-center h-[80vh]  text-center max-w-5xl mx-auto w-full px-4 mt-32"
         >
-          <p className="lg:text-6xl md:px-0 md:text-5xl lg:pt-10 text-n-13xl px-2 space-y-4 font-bold">
-            <div className="text-white/90">
-              The Next Generation
-              <br />
-              <span className="relative inline-block">
-                <AnimatedText text="AI Powered" />
-                <img
-                  src={curve}
-                  className="top-full absolute left-0 w-full"
-                  width={624}
-                  height={28}
-                  alt="Curve"
-                />
+          <div className="space-y-4">
+            <p className="lg:text-6xl md:px-0 md:text-5xl lg:pt-10 px-2 space-y-4 text-3xl font-bold">
+              <div className="text-white/90">
+                The Next Generation
+                <br />
+                <span className="relative inline-block">
+                  <AnimatedText text="AI Powered" />
+                  <img
+                    src={curve}
+                    className="top-full absolute left-0 w-full"
+                    width={624}
+                    height={28}
+                    alt="Curve"
+                  />
+                </span>
+              </div>
+
+              <span className="relative inline-block mt-2 text-orange-500">
+                Cloud Automation Platform
               </span>
-            </div>
+            </p>
 
-            <span className="relative inline-block mt-2 text-orange-500">
-              Cloud Automation Platform
-            </span>
-          </p>
+            <p className="body-1 opacity-80 w-full md:w-[90%] mx-auto md:text-xl">
+              HIAIDO is a powerful AI platform designed to revolutionize your
+              cloud operations, seamlessly automating tasks and amplifying
+              efficiency.
+              <br />
+              To make cloud management intuitive, we offer tailored solutions
+              and ongoing innovation, redefining seamless cloud experiences.
+              Experience efficiency like never before.
+            </p>
+          </div>
 
-          <p className="body-1 opacity-80 w-full md:w-[90%] mx-auto md:text-xl">
-            HIAIDO is a powerful AI platform designed to revolutionize your
-            cloud operations, seamlessly automating tasks and amplifying
-            efficiency.
-            <br />
-            To make cloud management intuitive, we offer tailored solutions and
-            ongoing innovation, redefining seamless cloud experiences.
-            Experience efficiency like never before.
-          </p>
+          <form onSubmit={handleSubmit} className="mt-20 space-y-4">
+            <p className="type1 lg:text-2xl md:text-sm text-xs font-bold">
+              &quot;welcome to the future of automation with HIAIDO&quot;
+            </p>
 
-          <p className="type1 lg:text-2xl md:text-sm text-xs font-bold">
-            &quot;welcome to the future of automation with HIAIDO&quot;
-          </p>
-
-          <form onSubmit={handleSubmit}>
             <div
               value={data.request_email}
               name="request_email"
-              className="md:mt-10 md:flex-row flex flex-col items-center justify-center gap-4"
+              className="md:flex-row flex flex-col items-center justify-center gap-4"
             >
               <input
                 onChange={handleInputChange}
-                className="input-placeholder decoration-none focus:outline-none placeholder:text-black bg-white/80 text-black/80 py-2 pl-6 font-semibold rounded-full"
+                className="input-placeholder decoration-none focus:outline-none placeholder:text-black bg-white/80 text-black/80 md:py-2 py-1 pl-6 font-semibold rounded-full"
                 type="text"
                 placeholder="hiaido@gmail.com"
               />
 
               <button
-                className="bg-orange-500/80 px-4 py-2 font-semibold rounded-full"
+                className="bg-orange-500/80 w-44 md:py-2 px-4 py-1 font-semibold rounded-full"
                 onClick={refreshPage}
                 type="submit"
               >
