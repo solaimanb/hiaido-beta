ec612c91d4570c05128f12f950207d64793a9185
soloman-err <soloman.err@gmail.com>
2024-05-15 13:34:05 +0600

    SEO functionalities update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import Benefits from "../components/page-components/homepage/Benefits";
 import Hero from "../components/page-components/homepage/Hero";
 import Contact from "../components/shared/Contact";
 
 const Landing = () => {
+  useEffect(() => {
+    document.title =
+      "Hiaido | AI Powered Cloud Operations Automation | Cloud Automation Platform India";
+  }, []);
+
   return (
     <div className="bg-black/90 min-h-screen px-2 overflow-hidden">
       <Hero />
