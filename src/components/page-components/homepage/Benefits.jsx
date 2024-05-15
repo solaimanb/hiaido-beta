10cfe91846dcaf8e4a5a124064cfeff772feee44
soloman-err <soloman.err@gmail.com>
2024-05-15 23:01:05 +0600

    redirection issue fix

--- src/components/page-components/homepage/Benefits.jsx
+++ src/components/page-components/homepage/Benefits.jsx
@@ -4,18 +4,51 @@ import Section from "../../Section";
 import azure from "../../../assets/images/azure.png";
 import gcp from "../../../assets/images/gcp.png";
 import "../../../index.css";
+import { useEffect, useRef } from "react";
+
+import { motion } from "framer-motion";
 
 const Benefits = () => {
+  const animateRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("animate");
+          } else {
+            entry.target.classList.remove("animate");
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (animateRef.current) {
+      observer.observe(animateRef.current);
+    }
+
+    return () => {
+      if (animateRef.current) {
+        observer.unobserve(animateRef.current);
+      }
+    };
+  }, []);
+
   return (
     <Section id="features">
-      <div className="md:pt-40 container relative min-h-screen pt-20 mt-20">
+      <div
+        ref={animateRef}
+        className="md:pt-40 container relative min-h-screen pt-20 mt-20"
+      >
         <div className="flex flex-col flex-wrap items-center justify-center gap-4 mb-8 font-bold">
           <p className="lg:text-4xl w-auto text-2xl text-center text-orange-400">
             Build Faster, Build Better, Build With AI
           </p>
-          <p className=" type2 lg:text-3xl text-center">
-            automate anything, almost, literally...
-          </p>
+          <motion.p className="type2 lg:text-xl text-center">
+            automate anything, almost, literally..
+          </motion.p>
         </div>
 
         <Heading
