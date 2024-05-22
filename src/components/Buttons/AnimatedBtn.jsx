6d6b5d43864cad77c3ffa5c914282662e54b4e71
soloman-err <soloman.err@gmail.com>
2024-05-23 01:16:07 +0600

    style: login btn animation update

--- src/components/Buttons/AnimatedBtn.jsx
+++ src/components/Buttons/AnimatedBtn.jsx
@@ -3,32 +3,31 @@ import "./AnimatedBtn.css";
 const AnimatedBtn = ({
   className,
   href,
-  onClick,
   children,
-  px,
   white,
   id,
   style,
   additionalClasses,
 }) => {
   return (
-    <div
+    <button
       id={id || "animated-btn"}
       className={`${additionalClasses}`}
       style={style}
+      // onClick={onClick}
     >
       <div
-        className={`z-50 px-${
-          px || 8
-        } py-2 text-center transition-all duration-200 ${
-          white ? "text-white" : ""
-        } ${className}`}
+        className={`child-wrapper ${white ? "text-white" : ""} ${className}`}
       >
-        <a href={href} onClick={onClick} className="text-container">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <a href={href} className="text-container">
           {children}
         </a>
       </div>
-    </div>
+    </button>
   );
 };
 
