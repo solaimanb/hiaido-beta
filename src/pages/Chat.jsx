596761b12882d1005ef00cd21a427ab2a599d97c
Moon Patel <moonpatel2003@gmail.com>
2024-05-24 20:17:00 +0530

    sidebat animated

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -27,6 +27,8 @@ import logo from "/hiaido-logo.png";
 import {
   ArrowUpTrayIcon,
   ChatBubbleLeftRightIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
   CloudArrowUpIcon,
   Cog6ToothIcon,
   CurrencyDollarIcon,
@@ -42,9 +44,19 @@ import {
   UsersIcon,
 } from "@heroicons/react/24/solid";
 import * as Menubar from "@radix-ui/react-menubar";
-import { motion } from "framer-motion";
+import {
+  AnimatePresence,
+  delay,
+  motion,
+  useAnimationControls,
+} from "framer-motion";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
-import { CopyIcon } from "@radix-ui/react-icons";
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowTopLeftIcon,
+  CopyIcon,
+} from "@radix-ui/react-icons";
 import toast from "react-hot-toast";
 
 const sampleChat = [
@@ -192,167 +204,260 @@ const Navbar = () => {
   const [navTabIndex, setNavTabIndex] = useState();
   const [activeTabIndex, setActiveTabIndex] = useState(1);
   const [isCollapsed, setIsCollapsed] = useState(true);
+  const controls = useAnimationControls();
 
   const data = [
-    { label: "Light", icon: <SunIcon className="w-6 pr-1" /> },
-    { label: "Dark", icon: <MoonIcon className="w-6 pr-1" /> },
+    { label: "Light", icon: <SunIcon className="w-5 " /> },
+    { label: "Dark", icon: <MoonIcon className="w-5 " /> },
   ];
   const sliceIndex = 8;
   const navbarData = [
-    { label: "Dashboard", icon: <Squares2X2Icon className="w-8 h-8 pr-3" /> },
+    { label: "Dashboard", icon: <Squares2X2Icon className="w-6" /> },
     {
       label: "Chat",
-      icon: <ChatBubbleLeftRightIcon className="w-8 h-8 pr-3" />,
+      icon: <ChatBubbleLeftRightIcon className="w-6" />,
     },
     {
       label: "Usage Analytics",
-      icon: <PresentationChartLineIcon className="w-8 h-8 pr-3" />,
+      icon: <PresentationChartLineIcon className="w-6" />,
     },
     {
       label: "Deployments",
-      icon: <CloudArrowUpIcon className="w-8 h-8 pr-3" />,
+      icon: <CloudArrowUpIcon className="w-6" />,
     },
-    { label: "Scheduler", icon: <QueueListIcon className="w-8 h-8 pr-3" /> },
+    { label: "Scheduler", icon: <QueueListIcon className="w-6" /> },
     {
       label: "Feature requests",
-      icon: <SquaresPlusIcon className="w-8 h-8 pr-3" />,
+      icon: <SquaresPlusIcon className="w-6" />,
     },
-    { label: "User management", icon: <UsersIcon className="w-8 h-8 pr-3" /> },
-    { label: "Billing", icon: <CurrencyDollarIcon className="w-8 h-8 pr-3" /> },
+    { label: "User management", icon: <UsersIcon className="w-6" /> },
+    { label: "Billing", icon: <CurrencyDollarIcon className="w-6" /> },
     {
       label: "Tickets",
-      icon: <TicketIcon className="w-8 h-8 pr-3" />,
+      icon: <TicketIcon className="w-6" />,
     },
-    { label: "Settings", icon: <Cog6ToothIcon className="w-8 h-8 pr-3" /> },
+    { label: "Settings", icon: <Cog6ToothIcon className="w-6" /> },
     {
       label: "Help",
-      icon: <QuestionMarkCircleIcon className="w-8 h-8 pr-3" />,
+      icon: <QuestionMarkCircleIcon className="w-6" />,
     },
-    // { label: "Pricing", icon: <CurrencyDollarIcon className="w-8 h-8 pr-3" /> },
-    // { label: "Settings", icon: <Cog6ToothIcon className="w-8 h-8 pr-3" /> },
+    // { label: "Pricing", icon: <CurrencyDollarIcon className="w-6" /> },
+    // { label: "Settings", icon: <Cog6ToothIcon className="w-6" /> },
   ];
 
+  const labelTransitions = {
+    transition: { delay: "0.3", ease: "easeIn" },
+    initial: {
+      opacity: 0,
+      transition: { delay: 0.2 },
+    },
+    animate: {
+      opacity: 1,
+    },
+    exit: {
+      opacity: 0,
+      transition: { delay: 0 },
+    },
+  };
+
   return (
-    <div
-      className={`bg-neutral-800 relative h-full rounded-lg text-[13px] flex flex-col ${
-        isCollapsed ? "w-[48px] p-1 py-3 justify-center" : "w-[210px] p-3"
-      }`}
-    >
-      <div className={`flex items-center my-4 text-2xl ${isCollapsed && "justify-center"}`}>
-        <img
-          onClick={() => setIsCollapsed(!isCollapsed)}
-          className={`w-8 h-8 ${isCollapsed || "mr-2"}`}
-          src={logo}
-          alt="Brand Logo"
-        />
-        {isCollapsed || <h1>HiAiDo</h1>}
-      </div>
-      <div className="divide-neutral-600 h-full">
-        <div className="text-neutral-400 my-2 space-y-1">
-          <div className="text-neutral-500 my-2 h-3 text-xs font-semibold">
-            {isCollapsed || "MAIN"}
-          </div>
-          {navbarData.slice(0, sliceIndex).map((item, i) => {
-            return (
-              <button
-                key={item.id || i}
-                onClick={() => setNavTabIndex(i)}
-                className={`rounded-lg flex items-center justify-center outline-none p-[1px] px-2 ${
-                  i == navTabIndex
-                    ? "bg-neutral-600 text-white"
-                    : "hover:bg-neutral-700 duration-300 hover:text-neutral-300"
-                }`}
-              >
-                {item.icon}
-                {isCollapsed || <span className="text-left">{item.label}</span>}
-              </button>
-            );
-          })}
+    <AnimatePresence mode="wait">
+      <motion.div
+        className={`bg-neutral-800 relative h-full rounded-lg text-[13px] flex flex-col ${
+          isCollapsed ? "p-1 py-3 " : "p-3"
+        }`}
+        variants={{
+          expand: {
+            width: "210px",
+          },
+          collapse: {
+            width: "48px",
+            transition: { delay: 0.3 },
+          },
+        }}
+        // transition={{ delay: 0.4 }}
+        animate={isCollapsed ? "collapse" : "expand"}
+      >
+        <div
+          className={`flex items-center justify-start my-4 text-2xl pl-1 ${""}`}
+        >
+          <img
+            className={`w-8 h-8 ${isCollapsed || "mr-2"}`}
+            src={logo}
+            alt="Brand Logo"
+          />
+          <AnimatePresence>
+            {isCollapsed || <motion.h1 {...labelTransitions}>HiAiDo</motion.h1>}
+          </AnimatePresence>
         </div>
-        <div className="h-[1px] bg-neutral-700/75 my-2"></div>
-        <div className="text-neutral-400 mt-3 h-full space-y-1">
-          <div className="text-neutral-500 my-2 h-3 text-xs font-semibold">
-            {isCollapsed || "SUPPORT"}
-          </div>
-          {navbarData.slice(sliceIndex).map((item, i) => {
-            return (
-              <button
-                key={item.id || i}
-                onClick={() => setNavTabIndex(i + 5)}
-                className={`rounded-lg flex items-center justify-center outline-none p-[1px] px-2 ${
-                  i + 5 == navTabIndex
-                    ? "bg-neutral-600 text-white"
-                    : "hover:bg-neutral-700 duration-300 hover:text-neutral-300"
-                }`}
-              >
-                {item.icon}
-                {isCollapsed || <span className="text-left">{item.label}</span>}
-              </button>
-            );
-          })}
-        </div>
-      </div>
-      <div className="flex flex-col h-fit items-center justify-end mx-3 space-y-2">
-        {isCollapsed ? (
-          <div className="">{data[parseInt(activeTabIndex)].icon}</div>
-        ) : (
-          <Menubar.Root className="flex bg-neutral-900 w-fit p-[5px] rounded-lg shadow-blackA4 space-x-1 justify-start">
-            {data.map((item, i) => {
+        <div className="divide-neutral-600 h-full">
+          <div className="text-neutral-400 my-2 space-y-[5px]">
+            <motion.div className="text-neutral-500 my-2 h-3 text-xs font-semibold">
+              <AnimatePresence>
+                {isCollapsed || (
+                  <motion.span {...labelTransitions}>MAIN</motion.span>
+                )}
+              </AnimatePresence>
+            </motion.div>
+            {navbarData.slice(0, sliceIndex).map((item, i) => {
               return (
-                <Menubar.Menu key={item.id || i}>
-                  <Menubar.Trigger
-                    onClick={() => {
-                      setActiveTabIndex(i);
-                    }}
-                    className={`py-[7px] w-fit px-[12px] outline-none select-none font-medium leading-none rounded-lg relative ${
-                      i == activeTabIndex ? "" : "hover:bg-neutral-600/35"
-                    }`}
-                    style={{
-                      WebkitTapHighlightColor: "transparent",
-                    }}
-                  >
-                    <div className="flex items-center justify-center">
-                      {item.icon}
-                      <span className="text-sm">{item.label}</span>
-                      {activeTabIndex == i && (
-                        <motion.span
-                          layoutId="theme-bubble"
-                          className="bg-cyan-100 mix-blend-difference absolute inset-0 z-10 rounded-lg"
-                          transition={{
-                            type: "spring",
-                            bounce: 0.1,
-                            duration: 0.6,
-                          }}
-                        />
-                      )}
-                    </div>
-                  </Menubar.Trigger>
-                </Menubar.Menu>
+                <button
+                  key={item.id || i}
+                  onClick={() => setNavTabIndex(i)}
+                  className={`rounded-lg flex items-center justify-start py-1 ${
+                    isCollapsed || "w-full"
+                  } outline-none p-[1px] px-2 ${
+                    i == navTabIndex
+                      ? "bg-neutral-600 text-white"
+                      : "hover:bg-neutral-700 duration-300 hover:text-neutral-300"
+                  }`}
+                >
+                  {item.icon}
+                  <AnimatePresence>
+                    {isCollapsed || (
+                      <motion.span
+                        className="text-left ml-3"
+                        {...labelTransitions}
+                      >
+                        {item.label}
+                      </motion.span>
+                    )}
+                  </AnimatePresence>
+                </button>
               );
             })}
-          </Menubar.Root>
-        )}
+          </div>
+          <div className="h-[1px] bg-neutral-700/75 my-2"></div>
+          <div className="text-neutral-400 mt-5 h-full space-y-[5px]">
+            <div className="text-neutral-500 my-2 h-3 text-xs font-semibold">
+              <AnimatePresence>
+                {isCollapsed || (
+                  <motion.span {...labelTransitions}>SUPPORT</motion.span>
+                )}
+              </AnimatePresence>
+            </div>
+            {navbarData.slice(sliceIndex).map((item, i) => {
+              return (
+                <button
+                  key={item.id || i}
+                  onClick={() => setNavTabIndex(i + 5)}
+                  className={`rounded-lg flex items-center justify-start outline-none p-[1px] px-2 py-1 ${
+                    isCollapsed || "w-full"
+                  } ${
+                    i + 5 == navTabIndex
+                      ? "bg-neutral-600 text-white"
+                      : "hover:bg-neutral-700 duration-300 hover:text-neutral-300"
+                  }`}
+                >
+                  <motion.span>{item.icon}</motion.span>
 
-        <div className="h-[1px] bg-neutral-600"></div>
-        <Flex
-          gap="2"
-          align="center"
-          className="!border-t-[1px] !border-neutral-600 pt-5 pb-1"
+                  <AnimatePresence>
+                    {isCollapsed || (
+                      <motion.span
+                        {...labelTransitions}
+                        className="text-left ml-3"
+                      >
+                        {item.label}
+                      </motion.span>
+                    )}
+                  </AnimatePresence>
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col h-fit items-center justify-end mx-3 space-y-2">
+          {
+            // isCollapsed ? (
+            //   <div className="">{data[parseInt(activeTabIndex)].icon}</div>
+            // ) :
+            <Menubar.Root className="flex bg-neutral-900 w-fit p-[7px] rounded-full shadow-blackA4 space-x-2 justify-start">
+              {data.map((item, i) => {
+                return (
+                  <AnimatePresence>
+                    {(isCollapsed && i != activeTabIndex) || (
+                      <motion.div {...labelTransitions}>
+                        <Menubar.Menu key={item.id || i}>
+                          <Menubar.Trigger
+                            onClick={() => {
+                              setActiveTabIndex(i);
+                            }}
+                            className={`p-2 outline-none select-none font-medium leading-none rounded-full relative ${
+                              i == activeTabIndex
+                                ? ""
+                                : "hover:bg-neutral-600/35"
+                            }`}
+                            style={{
+                              WebkitTapHighlightColor: "transparent",
+                            }}
+                          >
+                            <div className="flex items-center justify-center">
+                              {item.icon}
+                              <AnimatePresence>
+                                {isCollapsed || (
+                                  <motion.span
+                                    className="text-sm ml-2"
+                                    {...labelTransitions}
+                                  >
+                                    {item.label}
+                                  </motion.span>
+                                )}
+                              </AnimatePresence>
+                              {activeTabIndex == i && (
+                                <motion.span
+                                  layoutId="theme-bubble"
+                                  className="bg-cyan-100 mix-blend-difference absolute inset-0 z-10 rounded-full"
+                                  transition={{
+                                    type: "spring",
+                                    bounce: 0.1,
+                                    duration: 0.6,
+                                  }}
+                                />
+                              )}
+                            </div>
+                          </Menubar.Trigger>
+                        </Menubar.Menu>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                );
+              })}
+            </Menubar.Root>
+          }
+
+          <div className="h-[1px] bg-neutral-600"></div>
+          <Flex
+            gap="2"
+            align="center"
+            className="!border-t-[1px] !border-neutral-600 pt-5 pb-1"
+          >
+            <Avatar src="" fallback="U" radius="full" size="2" />
+            <AnimatePresence>
+              {isCollapsed || (
+                <motion.div className="max-w-[70%]" {...labelTransitions}>
+                  <Text truncate size={"2"}>
+                    Nadine Schtakieff
+                  </Text>
+                  <Text truncate size={"1"} className="text-neutral-500">
+                    Frontend Developer
+                  </Text>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </Flex>
+        </div>
+        <button
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          className="absolute top-16 -right-4 bg-neutral-700 p-2 rounded-full"
         >
-          <Avatar src="" fallback="U" radius="full" />
-          {isCollapsed || (
-            <Box maxWidth={"70%"}>
-              <Text truncate size={"2"}>
-                Nadine Schtakieff
-              </Text>
-              <Text truncate size={"1"} className="text-neutral-500">
-                Frontend Developer
-              </Text>
-            </Box>
+          {isCollapsed ? (
+            <ChevronRightIcon className="w-4 h-4" />
+          ) : (
+            <ChevronLeftIcon className="w-4 h-4" />
           )}
-        </Flex>
-      </div>
-    </div>
+        </button>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
