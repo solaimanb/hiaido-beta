29bdd4ef6c753f8f4d21de76fd30a19c33a3f9df
soloman-err <soloman.err@gmail.com>
2024-06-05 08:34:09 +0600

    update: animation functionality update

--- src/components/page-components/homepage/hero/InteractiveAnimation.jsx
+++ src/components/page-components/homepage/hero/InteractiveAnimation.jsx
@@ -56,7 +56,7 @@ const InteractiveAnimation = ({ showSecondAnimation }) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % activeTexts.length);
-    }, 5300);
+    }, 5100);
 
     return () => clearInterval(interval);
   }, [activeTexts]);
