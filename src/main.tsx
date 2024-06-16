d8d79859dcb492ea3b7d1e515f2015dd8fe042f4
Moon Patel <moonpatel2003@gmail.com>
2024-06-17 01:10:54 +0530

    add: typescript support

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+// import { StoreProvider } from "@/store/Store";
 import { StoreProvider } from "./store/Store.jsx";
 import { Theme } from "@radix-ui/themes";
 import { Authenticator, View } from "@aws-amplify/ui-react";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { GlobalStateProvider } from "./context/GlobalStateContext.jsx";
-import {ThemeProvider as TH} from '@aws-amplify/ui-react'
+import { ThemeProvider as TH } from "@aws-amplify/ui-react";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <StoreProvider>
