b6ccdf84ed01bbb3b4b56e185b0bf85022415ee1
Moon Patel <moonpatel2003@gmail.com>
2024-06-18 21:30:40 +0530

    fix

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export default { baseURL: "https://chat.hiaido.com", multiAgentURL:"http://54.205.170.241:8000" };
+export default { baseURL: "https://chat.hiaido.com", multiAgentURL:"https://api.hiaido.com" };
