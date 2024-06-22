91dbe36ec74a78b6eef1c851ee66372fc05c37d8
Moon Patel <moonpatel2003@gmail.com>
2024-06-22 18:11:21 +0530

    fix

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -1,13 +1,19 @@
-import logo from "/hiaido-logo.png";
+// import logo from "/hiaido-logo.png";
+
+// const Loading = () => {
+//   return (
+//     <div className="w-screen h-screen absolute bg-neutral-800 flex justify-center items-center">
+//       <div className="flex-col flex">
+//         <img src={logo} alt="Hiaido logo" className="size-40 animate-bounce" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Loading;
 
 const Loading = () => {
-  return (
-    <div className="w-screen h-screen absolute bg-neutral-800 flex justify-center items-center">
-      <div className="flex-col flex">
-        <img src={logo} alt="Hiaido logo" className="size-40 animate-bounce" />
-      </div>
-    </div>
-  );
+  return <div className="bg-dark w-screen h-screen"></div>;
 };
 
 export default Loading;
