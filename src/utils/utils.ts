33118d8db5cdde3d31b9edefcd89b186c15eb76c
Moon Patel <moonpatel2003@gmail.com>
2024-08-03 01:16:49 +0530

    add: utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { ConnectedAccount, ManagedMemberAccount } from "@/types";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+function isManagedMemberAccount(obj: any): obj is ManagedMemberAccount {
+  return obj;
+}
+
+function isConnectedAccount(obj: any): obj is ConnectedAccount {
+  return obj;
 }
